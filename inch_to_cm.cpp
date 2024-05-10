005dabc5968566c4db2c5692bb21486059e9a4e0
TPedreira <tgo.pedreira@gmail.com>
2024-05-10 20:39:53 +0100

    Added inch to cm function

---
+++ inch_to_cm.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    // while (condition) {body}
+    float inch, centimeter;
+    inch = 1.;
+    while (inch <= 10.1) 
+        {
+            centimeter = 2.56 * inch;
+            printf("%i inches are %f centimeters\n", inch, centimeter);
+            inch = inch + 1.;
+        }
+    return 0;
+}
