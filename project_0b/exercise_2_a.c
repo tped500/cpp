34e559f63ad487e54c8e93b6aa6adc79baa4374c
TPedreira <tgo.pedreira@gmail.com>
2024-06-01 20:27:41 +0100

    Refactored function and reformatted

--- project_0b/exercise_2_a.c
+++ project_0b/exercise_2_a.c
@@ -11,17 +11,14 @@ F = C × 1.8 + 32
 
 #include <stdio.h>
 
-void celsius_to_fahrenheit(double celsius) {
-  printf("Converting %.2lf Celsius to Fahrenheit\n", celsius);
-  double fahrenheit = celsius * 1.8 + 32;
-  printf("%.2lfC is equivalent to %.2lfF\n", celsius, fahrenheit);
-}
-
 int main() {
   double celsius;
   printf("How many Celsius do you want to convert? ");
   scanf("%lf", &celsius);
-  celsius_to_fahrenheit(celsius=celsius);
+
+  printf("Converting %.2lf Celsius to Fahrenheit\n", celsius);
+  const fahrenheit = celsius * 1.8 + 32;
+  printf("%.2lfC is equivalent to %.2lfF\n", celsius, fahrenheit);
 
   return 0;
 }
