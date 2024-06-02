81c0072fbe4bf57a2216d980bc941bf4adcd5804
TPedreira <tgo.pedreira@gmail.com>
2024-06-02 19:51:34 +0100

    Changed logic of main and temperature_converter

--- project_0b/exercise_2_b_c_d_e.c
+++ project_0b/exercise_2_b_c_d_e.c
@@ -16,64 +16,59 @@ C = (32 - F)/1.8
 #include <ctype.h>
 #include <stdio.h>
 
-int temperature_converter(double temperature, char scale) {
-  double converted_temp;
-
-  double min_c = -273.15;
-  double min_f = -459.67;
-
-  if (scale == 'C' && temperature < min_c) {
-    printf("\n\nThere are no temperatures lower than %lfC.\n", min_c);
-    return 1;
-  } else if (scale == 'F' && temperature < min_f) {
-    printf("\n\nThere are no temperatures lower than %lfF.\n", min_f);
-    return 1;
-  } else if (temperature == 'e') {
-    printf("\n\nThank you for using temperature converter.\n");
-    return 0;
-  }
-
+void temperature_converter(double temperature, char scale) {
   if (scale == 'C') {
     printf("\n-> Converting %.2lfF to C\n", temperature);
-    converted_temp = (32 - temperature) / 1.8;
+    const double converted_temp = (32 - temperature) / 1.8;
     printf("---> %.2lfF is equivalent to %.2lfC\n", temperature,
            converted_temp);
   } else {
     printf("\n-> Converting %.2lfC to F\n", temperature);
-    converted_temp = temperature * 1.8  + 32;
+    const double converted_temp = temperature * 1.8 + 32;
     printf("---> %.2lfC is equivalent to %.2lfF\n", temperature,
            converted_temp);
   }
-  return 1;
 }
 
 int main() {
-  char scale;
-
   while (1) {
     printf(
         "\nWhich scale do you want to convert to? Please input one of the "
         "following:\n(F) - converts from C to F\n(C) - converts from F to "
         "C\nIf you wish to stop, type 'E'.\n: ");
+    char scale;
     scanf(" %c", &scale);
+    scale = toupper(scale);
 
-    if (toupper(scale) != 'C' && toupper(scale) != 'F' &&
-        toupper(scale) != 'E' && toupper(scale) != '\n') {
+    if (scale != 'C' && scale != 'F' && scale != 'E') {
       printf("\n\nSorry, but only C or F are accepted. Please try again.\n");
       continue;
     }
 
-    if (toupper(scale) == 'E') {
+    if (scale == 'E') {
       printf("\n\nThank you for using temperature converter.\n");
       break;
+
     } else {
       double temp_to_convert;
 
       printf("\nHow many degrees do you want to convert? ");
       scanf("%lf", &temp_to_convert);
 
-      if (!temperature_converter(temp_to_convert, toupper(scale))) {
+      const double min_celsius = -273.15;
+      const double min_fahrenheit = -459.67;
+
+      if (scale == 'C' && temp_to_convert < min_celsius) {
+        printf("\n\nThere are no temperatures lower than %.2lfC.\n", min_celsius);
+        continue;
+      } else if (scale == 'F' && temp_to_convert < min_fahrenheit) {
+        printf("\n\nThere are no temperatures lower than %.2lfF.\n", min_fahrenheit);
+        continue;
+      } else if (toupper(temp_to_convert) == 'E') {
+        printf("\n\nThank you for using temperature converter.\n");
         break;
+      } else {
+        temperature_converter(temp_to_convert, scale);
       }
     }
   }
