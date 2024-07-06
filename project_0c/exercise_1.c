e37d5f7697c9d0463c56a0e3f3eca62a804de53f
TPedreira <tgo.pedreira@gmail.com>
2024-07-06 16:28:25 +0100

    Exercide 1c

---
+++ project_0c/exercise_1.c
@@ -0,0 +1,33 @@
+/*
+Remake exercise 5 from Project 0a and print values on screen and save them to a file.
+*/
+
+
+#include <stdio.h>
+
+int main() {
+
+  int number;
+  printf("\nFor which number would you like to see the multiplication table?\n");
+  scanf("%d", &number);
+
+  char filename[100];
+  printf("\nWrite an output file name:\n");
+  scanf("%s", filename);
+
+  FILE *file_in = fopen(filename, "w");
+
+  printf("\nMultiplication table for %d\n", number);
+  
+  int factor = 0;
+  while (factor <= 10) {
+    int calculation = number * factor;
+    printf("%d x %d = %d\n", number, factor, calculation);
+    fprintf(file_in, "%d x %d = %d\n", number, factor, calculation);
+    factor = factor + 1;
+  }
+
+  printf("Wrote file: %s\n", filename);
+
+  return 0;
+}
