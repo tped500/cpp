21a950678f1efc62c645c8bd9dc3625d03f817cb
TPedreira <tgo.pedreira@gmail.com>
2024-07-06 16:35:06 +0100

    formatting

--- project_0c/exercise_1.c
+++ project_0c/exercise_1.c
@@ -1,14 +1,14 @@
 /*
-Remake exercise 5 from Project 0a and print values on screen and save them to a file.
+Remake exercise 5 from Project 0a and print values on screen and save them to a
+file.
 */
-
 
 #include <stdio.h>
 
 int main() {
-
   int number;
-  printf("\nFor which number would you like to see the multiplication table?\n");
+  printf(
+      "\nFor which number would you like to see the multiplication table?\n");
   scanf("%d", &number);
 
   char filename[100];
@@ -18,7 +18,7 @@ int main() {
   FILE *file_in = fopen(filename, "wt");
 
   printf("\nMultiplication table for %d\n", number);
-  
+
   int factor = 0;
   while (factor <= 10) {
     int calculation = number * factor;
@@ -26,7 +26,7 @@ int main() {
     fprintf(file_in, "%d x %d = %d\n", number, factor, calculation);
     factor = factor + 1;
   }
-  
+
   fclose(file_in);
   printf("Wrote file: %s\n", filename);
 
